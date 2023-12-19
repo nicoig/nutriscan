56f7bbb2c4923a15ae31a3a79b279609567eb77b
Nicolás Ignacio Guerra Villalobos <enguerra@Carozzicorp.com>
2023-12-19 09:15:22 -0300

    Initial commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 # Primera Carga a Github
 # git init
 # git add .
-# git remote add origin https://github.com/nicoig/ecoGPT.git
+# git remote add origin https://github.com/nicoig/LabelGPT.git
 # git commit -m "Initial commit"
 # git push -u origin master
 
@@ -17,113 +17,115 @@
 # agregar en variables de entorno
 # PYTHON_VERSION = 3.9.12
 
-# git remote set-url origin https://github.com/nicoig/ecoGPT.git
+# git remote set-url origin https://github.com/nicoig/LabelGPT.git
 # git remote -v
 # git push -u origin main
 
 
 ################################################
-#####
-
-
 
 
 import streamlit as st
 import base64
 from langchain.chat_models import ChatOpenAI
 from langchain.schema.messages import HumanMessage, AIMessage
-from dotenv import load_dotenv, find_dotenv
-from openai import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.schema import StrOutputParser
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-# Cargar las variables de entorno para las claves API
-load_dotenv(find_dotenv())
+# Cargar variables de entorno
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
 # Función para codificar imágenes en base64
 def encode_image(image_file):
     return base64.b64encode(image_file.read()).decode('utf-8')
 
-# Configura el título y subtítulo de la aplicación en Streamlit
-st.title("EcoGPT")
+# Función Texto a Voz
+def tts(text):
+    if api_key == '':
+        st.error('Por favor, configura tu clave API de OpenAI en el archivo .env')
+        return None
+    else:
+        try:
+            client = OpenAI(api_key=api_key)
+            response = client.audio.speech.create(
+                model="tts-1",  # Modelo por defecto
+                voice="echo",   # Voz por defecto
+                input=text,
+            )
+            return response.content
+        except Exception as error:
+            st.error(f"Se produjo un error al generar el habla: {error}")
+            return None
+
+# Función para convertir audio a base64 y crear elemento HTML para audio
+def get_audio_file_content(audio_content):
+    base64_audio = base64.b64encode(audio_content).decode('utf-8')
+    audio_html = f'<audio controls autoplay><source src="data:audio/mp3;base64,{base64_audio}" type="audio/mpeg"></audio>'
+    return audio_html
+
+# Diseño de la Aplicación Streamlit para LabelGPT
+st.title("LabelGPT")
+
+# Configura el subtítulo de la aplicación con un tamaño de fuente más pequeño
 st.markdown("""
     <style>
-    .small-font {
-        font-size:18px !important;
+    .subheader-font {
+        font-size:16px !important;
     }
     </style>
-    <p class="small-font">Soy EcoGPT, carga la imagen del producto que deseas reciclar y te daré algunos consejos</p>
     """, unsafe_allow_html=True)
 
-# Imagen de cabecera
-st.image('img/robot.png', width=250)
+st.markdown('<p class="subheader-font">LabelGPT: tu asistente IA para entender etiquetas nutricionales y seleccionar alimentos para dietas especiales.</p>', unsafe_allow_html=True)
+
+# Imagen
+st.image('img/app.png', width=250)
+
 
 # Carga de imagen por el usuario
-uploaded_file = st.file_uploader("Carga una imagen del producto que deseas reciclar", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader("Sube una imagen de la etiqueta del producto", type=["jpg", "png", "jpeg"])
 
-# Restablecer el contenido generado al cargar una nueva imagen
-if 'last_uploaded_file' not in st.session_state or (uploaded_file is not None and uploaded_file != st.session_state['last_uploaded_file']):
-    st.session_state['last_uploaded_file'] = uploaded_file
-    st.session_state['generated_content'] = False
+# Input para condiciones dietéticas específicas
+user_condition = st.text_input("Ingresa cualquier condición de salud o restricción dietética (opcional):")
 
-# Botón de enviar y proceso principal
-if st.button("Analizar Producto") and uploaded_file is not None:
-    with st.spinner('Identificando el producto y material...'):
+if st.button("Analizar Etiqueta") and uploaded_file is not None:
+    with st.spinner('Transcribiendo y analizando la etiqueta...'):
         image = encode_image(uploaded_file)
-        st.session_state['generated_content'] = True
-
-        # Identificar el producto y el material con la IA
         chain = ChatOpenAI(model="gpt-4-vision-preview", max_tokens=1024)
-        msg = chain.invoke(
-            [AIMessage(content="Basándose en la imagen, identifique el producto y el tipo de material."),
-             HumanMessage(content=[{"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image}"}}])
+        
+        # Transcribir la etiqueta
+        transcripcion_msg = chain.invoke(
+            [AIMessage(content="Por favor, identifique la información nutricional y los ingredientes que se muestran en la siguiente imagen. Incluya todos los detalles como calorías, grasas, carbohidratos, proteínas, vitaminas, minerales, y la lista completa de ingredientes."),
+            HumanMessage(content=[{"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image}"}}])
             ]
         )
+        st.session_state['transcripcion_etiqueta'] = transcripcion_msg.content
+        st.markdown("**Transcripción de la Etiqueta:**")
+        st.write(st.session_state['transcripcion_etiqueta'])
 
-        st.session_state['identificacion'] = msg.content
-        st.markdown("**Identificación del producto y material:**")
-        st.write(st.session_state['identificacion'])
-
-        # Generar recomendaciones de reciclaje
-        prompt_reciclaje = PromptTemplate.from_template(
+        # Analizar ingredientes en relación a la condición del usuario
+        prompt_analisis_ingredientes = PromptTemplate.from_template(
             """
-            Dado el siguiente producto y material identificado:
-            {identification}
-            ¿Qué consejos de reciclaje se pueden dar para este producto?
+            Dada la siguiente transcripción de una etiqueta nutricional:
+            {transcription}
+            y considerando la condición dietética específica: {condition}
+            ¿Hay algún ingrediente que podría ser perjudicial para la salud del usuario según su condición?
 
             Output:
             """
         )
-        runnable = prompt_reciclaje | chain | StrOutputParser()
-        st.session_state['consejos_reciclaje'] = runnable.invoke({"identification": st.session_state['identificacion']})
-        st.markdown("**Consejos para reciclar este producto:**")
-        st.write(st.session_state['consejos_reciclaje'])
-        
-        # Generar información sobre el impacto ecológico
-        prompt_impacto_ecologico = PromptTemplate.from_template(
-            """
-            Dado el siguiente producto y material identificado:
-            {identification}
-            Proporcione información sobre el tiempo de biodegradación del material y el impacto positivo de reciclar este material.
+        runnable = prompt_analisis_ingredientes | chain | StrOutputParser()
+        st.session_state['analisis_ingredientes'] = runnable.invoke({"transcription": st.session_state['transcripcion_etiqueta'], "condition": user_condition})
+        st.markdown("**Análisis de Ingredientes Según la Condición Ingresada:**")
+        st.write(st.session_state['analisis_ingredientes'])
 
-            Output:
-            """
-        )
-        runnable = prompt_impacto_ecologico | chain | StrOutputParser()
-        st.session_state['impacto_ecologico'] = runnable.invoke({"identification": st.session_state['identificacion']})
-        st.markdown("**Impacto Ecológico del Material:**")
-        st.write(st.session_state['impacto_ecologico'])
-
-# Función para compilar la información en un string
-def compile_information():
-    info = ""
-    if st.session_state.get('generated_content', False):
-        info += "Identificación del producto y material:\n" + st.session_state.get('identificacion', '') + "\n\n"
-        info += "Consejos para reciclar este producto:\n" + st.session_state.get('consejos_reciclaje', '') + "\n\n"
-        info += "Impacto Ecológico del Material:\n" + st.session_state.get('impacto_ecologico', '') + "\n\n"
-    return info
-
-# Botón para descargar la información
-if st.session_state.get('generated_content', False):
-    info_to_download = compile_information()
-    st.download_button(label="Descargar Información", data=info_to_download, file_name="ecoGPT_info.txt", mime="text/plain")
+        # Generar y reproducir audio automáticamente
+        texto_para_audio = st.session_state['analisis_ingredientes']
+        if texto_para_audio:
+            audio_content = tts(texto_para_audio)
+            if audio_content is not None:
+                audio_html = get_audio_file_content(audio_content)
+                st.markdown(audio_html, unsafe_allow_html=True)
